1413885ca986211499e516acf470bb572ee84f5b
pascal <pascal@lamy.mobi>
2023-09-20 16:13:22 +0200

    correction avec Accueil

--- app/src/main/java/esaip/angers/Accueil.java
+++ app/src/main/java/esaip/angers/Accueil.java
@@ -18,36 +18,11 @@ import esaip.angers.databinding.ActivityAccueilBinding;
 
 public class Accueil extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityAccueilBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityAccueilBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_accueil);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_accueil);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_accueil);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
+
