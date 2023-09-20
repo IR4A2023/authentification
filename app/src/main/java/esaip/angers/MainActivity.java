034286f5204b3970dc1ccbf736e59e8008d9407c
pascal <pascal@lamy.mobi>
2023-09-20 16:26:34 +0200

    correction avec authentification (toto - titi)

--- app/src/main/java/esaip/angers/MainActivity.java
+++ app/src/main/java/esaip/angers/MainActivity.java
@@ -4,6 +4,7 @@ import static android.widget.Toast.LENGTH_SHORT;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -20,8 +21,17 @@ EditText login, mdp;
         mdp=findViewById(R.id.pwd);
     }
     public void clickValider(View v){
-        Toast t= Toast.makeText(MainActivity.this, "Bonjour", Toast.LENGTH_SHORT);
-        t.show();
+        if(login.getText().toString().equals(getResources().getString(R.string.loginok)) &&
+        mdp.getText().toString().equals(getResources().getString(R.string.mdpok))){
+            Intent i= new Intent(MainActivity.this, Accueil.class);
+            i.putExtra("msg",login.getText().toString());
+            startActivity(i);
+        }
+        else {
+            Toast t = Toast.makeText(MainActivity.this, "Erreur Authentification", Toast.LENGTH_SHORT);
+            t.show();
+            this.effacer(v);
+        }
     }
     public void effacer(View v){
 
